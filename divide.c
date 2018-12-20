30b98943b74d326cde7e1171c6097d62e607ee5c
Brian Gu <gxh1991@gmail.com>
2018-12-19 21:02:13 -0600

    finished divide.c

--- divide.c
+++ divide.c
@@ -5,35 +5,36 @@ void divide(int a, int b)
 {
     // valid input b != 0?
     if (b == 0)  {
-        printf("The divisor cannot be 0");
-        return;
-    }
-    if (a < b) {
-        printf("%d",0);
+        printf("The divisor cannot be 0 \n");
         return;
     }
 
     float ans = (float)a / b;
-    ans = (int)(ans * 100) / 100.0;
-    // a/b == Integer or float?
-    if (ans - (int)ans == 0.0) {
-        printf("%d", (int)ans);
-    } else if(ans*10 - (int)(ans*10) == 0.0) {
-        printf("%d", (int)ans);
-        float first = ans*10 - (int)(ans*10);
-        printf(".");
-        printf("%d",(int)first);
-    } else {
-        printf("%d", (int)ans);
-        printf(".");
-        float first = ans*10 - (int)(ans*10);
-        printf("%d",(int)first);
-        float second = first*10 - (int)(first*10);
-        printf("%d",(int)second);
+    int ans_int = (int)(ans * 100);
+    char out[3] = {'0','0','0'};
+    for (int i=2;i>=0;i--) {
+        out[i] = (char)(ans_int - ans_int/10*10);
+        ans_int = ans_int/10;
+    }
+    printf("%d",out[0]);
+    if (out[1] == 0 && out[2] == 0) {
+        printf("\n");
+        return;
+    }
+    printf(".");
+    printf("%d",out[1]);
+    if (out[2] != 0) {
+        printf("%d",out[2]);
     }
     printf("\n");
 }
 
-int main() {
-    divide(8,5);
-}
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("The division needs TWO arguments!");
+    }
+    int a = argv[0] - '0';
+    int b = argv[1] - '0';
+    divide(a,b);
+    exit();
+ }
