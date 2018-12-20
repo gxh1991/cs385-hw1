2db6e9e32cff1b495bb24e807a31fe7b0bda6637
Brian Gu <gxh1991@gmail.com>
2018-12-20 11:44:38 -0600

    fixed makefile and bugs in divide.c

--- divide.c
+++ divide.c
@@ -1,40 +1,59 @@
-// #include "user.h"
-#include <stdio.h>
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
 
 void divide(int a, int b) 
 {
     // valid input b != 0?
     if (b == 0)  {
-        printf("The divisor cannot be 0 \n");
+        printf(1,"The divisor cannot be 0 \n");
         return;
     }
-
-    float ans = (float)a / b;
-    int ans_int = (int)(ans * 100);
-    char out[3] = {'0','0','0'};
+    int ans = a*100 / b;
+    // int ans_int = (int)(ans * 100);
+    int ans_int = ans;
+    int out[3] = {0,0,0};
     for (int i=2;i>=0;i--) {
-        out[i] = (char)(ans_int - ans_int/10*10);
+        out[i] = (ans_int - ans_int/10*10);
         ans_int = ans_int/10;
     }
-    printf("%d",out[0]);
+    printf(1,"%d",out[0]);
     if (out[1] == 0 && out[2] == 0) {
-        printf("\n");
+        printf(1,"\n");
         return;
     }
-    printf(".");
-    printf("%d",out[1]);
+    printf(1,".");
+    printf(1,"%d",out[1]);
     if (out[2] != 0) {
-        printf("%d",out[2]);
+        printf(1,"%d",out[2]);
     }
-    printf("\n");
+
+    printf(1,"\n");
+}
+
+int
+ctoi(char *s)
+{
+  int n;
+
+  n = 0;
+  while('0' <= *s && *s <= '9')
+    n = n*10 + *s++ - '0';
+  return n;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("The division needs TWO arguments!");
+    if (argc != 3) {
+        printf(1,"The division needs TWO arguments!");
+        exit();
+        return 0;
     }
-    int a = argv[0] - '0';
-    int b = argv[1] - '0';
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    printf(1,"%d\n",a);
+    printf(1,"%d\n",b);
     divide(a,b);
     exit();
+    return 0;
  }
